dc58f57bb025cf598b4e5cb84150246900f37543
yuma0313 <y.yuma0313@gmail.com>
2023-09-21 19:27:15 +0900

    出力の修正

--- app.go
+++ app.go
@@ -8,7 +8,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 		case http.MethodGet:
-			fmt.Fprintf(w, "GETメソッドaaaa")
+			fmt.Fprintf(w, "GETメソッド")
 		case http.MethodPost:
 			fmt.Fprintf(w, "POSTメソッド")
 		case http.MethodPut:
