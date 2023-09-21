9e23dfffb4408051cbc116c8e0ef8f135a68f539
yuma0313 <y.yuma0313@gmail.com>
2023-09-21 19:24:55 +0900

    first commit

---
+++ app.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+		case http.MethodGet:
+			fmt.Fprintf(w, "GETメソッドaaaa")
+		case http.MethodPost:
+			fmt.Fprintf(w, "POSTメソッド")
+		case http.MethodPut:
+			fmt.Fprintf(w, "PUTメソッド")
+		case http.MethodDelete:
+			fmt.Fprintln(w, "DELETEメソッド")
+		default:
+			http.Error(w, "メソッドがサポートされていません", http.StatusMethodNotAllowed)
+	}
+}
+
+func main() {
+	http.HandleFunc("/hello", helloHandler)
+	http.ListenAndServe(":8080", nil)
+}
